155f7b99be2da79750fc0e53006a5cd4f2c75b1c
divesh lunker <diveshdj92@gmail.com>
2019-04-01 23:41:11 +0530

    Add files via upload

---
+++ dash.php
@@ -0,0 +1,10 @@
+<html>
+    options:
+    1. all users
+    2. all organizations
+    3. all sponsors
+    4. money left for sponsoring events
+    5 request of sponsors
+    6. urgent requests of blood
+    7. blood availability
+</html>
